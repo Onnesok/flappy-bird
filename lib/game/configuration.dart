959c70bf556028ec852051d993dc7f62fc9b8a2f
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-07-27 22:41:14 +0600

    flappy_bird

--- lib/game/configuration.dart
+++ lib/game/configuration.dart
@@ -1,8 +1,8 @@
 class Config {
   static const groundHeight = 110.0;
-  static const gameSpeed = 200.0;
-  static const pipeInterval = 1.5;
-  static const birdVelocity = 210;
+  static const gameSpeed = 150.0;
+  static const pipeInterval = 2.0;
+  static const birdVelocity = 300;
   static const gravity = -100.0;
   static const cloudsHeight = 70.0;
 }
